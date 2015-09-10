5068083145f1b19b786d41c84f07b93b2aee0468
phildebrandt <phildebrandt@gmail.com>
2015-09-09 21:35:09 -0400

    events post type archive link

--- wp-content/themes/ucusergroup/page-home.php
+++ wp-content/themes/ucusergroup/page-home.php
@@ -108,7 +108,7 @@
                 wp_reset_postdata();
               ?>
             </ul>
-            <button type="button" class="btn btn-success"><i class="icon-search"></i>View all meetups</button>
+            <a href="<?php echo get_post_type_archive_link('espresso_events'); ?>" type="button" class="btn btn-success"><i class="icon-search"></i>View all meetups</a>
 
           </div>
         </div>
