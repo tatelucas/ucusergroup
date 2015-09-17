f293964fc1dcf824e5ca588d63c465512b0ee219
tatelucas <tate.lucas@gmail.com>
2015-09-17 19:30:47 -0400

    add compass, fix sponsors

---
+++ wp-content/themes/ucusergroup/config.rb
@@ -0,0 +1,14 @@
+# Compass CSS framework config file
+
+project_type = :stand_alone
+# Set this to the root of your project when deployed:
+http_path = "/"
+sass_dir = "scss"
+css_dir = "css"
+images_dir = "images"
+fonts_dir = "fonts"
+javascripts_dir = "js"
+line_comments = false
+preferred_syntax = :scss
+output_style = :expanded
+relative_assets = true
