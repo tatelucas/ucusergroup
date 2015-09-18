502fed48a081a83124fae5a87540a5d72dd80844
tatelucas <tate.lucas@gmail.com>
2015-09-17 20:38:43 -0400

    page formatting, about

--- wp-content/themes/ucusergroup/page.php
+++ wp-content/themes/ucusergroup/page.php
@@ -17,17 +17,35 @@
 <?php get_template_parts( array( 'parts/shared/html-header', 'parts/shared/header' ) ); ?>
 
 <div class="middle">
-  <div class="content container-fluid">
-    <div class="row">
-      <div class="col-sm-12">
-        <?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
-        <h2><?php the_title(); ?></h2>
-        <?php the_content(); ?>
-        <?php comments_template( '', true ); ?>
-        <?php endwhile; ?>
-      </div>
+  <?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
+
+    <div class="page-title">
+      <div class="container-fluid">
+        <div class="row">
+          <div class="col-sm-12">
+            <h2><?php the_title(); ?></h2>
+            <?php
+            $subtitle = get_field( 'subtitle' );
+            if ($subtitle) {
+              echo "<p class='subtitle'>
+              $subtitle
+              </p>";
+            }
+            ?>
+          </div><!--/col-sm-12-->
+        </div><!--/row-->
+      </div><!--/container-fluid -->
     </div>
-  </div><!-- /content -->
+
+    <div class="content container-fluid">
+      <div class="row">
+        <div class="col-sm-12">
+          <?php the_content(); ?>
+          <?php comments_template( '', true ); ?>
+        </div>
+      </div>
+    </div><!-- /content -->
+  <?php endwhile; ?>
 </div><!--/middle-->
 
 
