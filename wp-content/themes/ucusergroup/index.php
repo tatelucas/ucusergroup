0c27a7b922b354836693fb8f558b14361f6113dd
tatelucas <tate.lucas@gmail.com>
2015-09-22 10:24:30 -0400

    single post page

--- wp-content/themes/ucusergroup/index.php
+++ wp-content/themes/ucusergroup/index.php
@@ -4,7 +4,7 @@
  * This is the most generic template file in a WordPress theme
  * and one of the two required files for a theme (the other being style.css).
  * It is used to display a page when nothing more specific matches a query.
- * E.g., it puts together the home page when no home.php file 
+ * E.g., it puts together the home page when no home.php file
  *
  * Please see /external/starkers-utilities.php for info on get_template_parts()
  *
@@ -15,9 +15,31 @@
 ?>
 <?php get_template_parts( array( 'parts/shared/html-header', 'parts/shared/header' ) ); ?>
 
+<div class="middle">
 <?php if ( have_posts() ): ?>
-<h2>Latest Posts</h2>	
-<ol>
+
+	<div class="page-title">
+		<div class="container-fluid">
+			<div class="row">
+				<div class="col-sm-12">
+					<h2>News & Updates</h2>
+					<?php
+					$subtitle = get_field( 'subtitle' );
+					if ($subtitle) {
+						echo "<p class='subtitle'>
+						$subtitle
+						</p>";
+					}
+					?>
+				</div><!--/col-sm-12-->
+			</div><!--/row-->
+		</div><!--/container-fluid -->
+	</div>
+
+	<div class="content container-fluid">
+		<div class="row">
+			<div class="col-sm-9">
+
 <?php while ( have_posts() ) : the_post(); ?>
 	<li>
 		<article>
@@ -31,5 +53,12 @@
 <?php else: ?>
 <h2>No posts to display</h2>
 <?php endif; ?>
+</div>
 
+<div class="sidebar-container col-sm-3">
+	<?php get_sidebar(); ?>
+</div><!--/col-sm-4-->
+</div>
+</div><!-- /content -->
+</div><!--/middle-->
 <?php get_template_parts( array( 'parts/shared/footer','parts/shared/html-footer') ); ?>
--- wp-content/themes/ucusergroup/single.php
+++ wp-content/themes/ucusergroup/single.php
@@ -11,23 +11,45 @@
 ?>
 <?php get_template_parts( array( 'parts/shared/html-header', 'parts/shared/header' ) ); ?>
 
-<?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
+<div class="middle">
+	<?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
 
-<article>
 
-	<h2><?php the_title(); ?></h2>
-	<time datetime="<?php the_time( 'Y-m-d' ); ?>" pubdate><?php the_date(); ?> <?php the_time(); ?></time> <?php comments_popup_link('Leave a Comment', '1 Comment', '% Comments'); ?>
-	<?php the_content(); ?>			
+		<div class="page-title">
+      <div class="container-fluid">
+        <div class="row">
+          <div class="col-sm-12">
+            <div class="row">
+							<div class="col-sm-10">
+								<h2><?php the_title(); ?></h2>
+								<div class="subtitle">
+									<time  datetime="<?php the_time( 'Y-m-d' ); ?>" pubdate><?php the_date(); ?></time>
+								</div><!--/subtitle-->
+							</div><!--/col-sm-10-->
+							<div class="col-sm-2 btn-group">
+								<?php comments_popup_link('Leave a Comment', '1 Comment', '% Comments', 'btn btn-outlined pull-right'); ?>
+							</div><!--/col-sm-2-->
+						</div><!--/row-->
+          </div><!--/col-sm-12-->
+        </div><!--/row-->
+      </div><!--/container-fluid -->
+    </div>
 
-	<?php if ( get_the_author_meta( 'description' ) ) : ?>
-	<?php echo get_avatar( get_the_author_meta( 'user_email' ) ); ?>
-	<h3>About <?php echo get_the_author() ; ?></h3>
-	<?php the_author_meta( 'description' ); ?>
-	<?php endif; ?>
+		<div class="content container-fluid">
+      <div class="row">
+        <div class="col-sm-9">
 
-	<?php comments_template( '', true ); ?>
+				<?php the_content(); ?>
+				<?php comments_template( '', true ); ?>
 
-</article>
-<?php endwhile; ?>
+			</div><!--/col-sm-9-->
+
+				<div class="sidebar-container col-sm-3">
+					<?php get_sidebar(); ?>
+				</div><!--/col-sm-3-->
+			</div><!--/row-->
+	</div><!-- /content -->
+	<?php endwhile; ?>
+</div><!--/middle-->
 
 <?php get_template_parts( array( 'parts/shared/footer','parts/shared/html-footer' ) ); ?>
