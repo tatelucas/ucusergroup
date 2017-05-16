2e2c1f9d7912a5637ce2e510bc84a78cc94b9bf2
dwlorimer <dwlorimer@gmail.com>
2017-05-16 09:50:04 -0400

    Show past events on city pages

--- wp-content/themes/ucusergroup/taxonomy-city.php
+++ wp-content/themes/ucusergroup/taxonomy-city.php
@@ -47,6 +47,7 @@
 				global $wp_query;
 				$wp_query = new EE_Event_List_Query( $attsNextThreeEvents );
 				if ( have_posts() ): ?>
+				<h1>Upcoming Events:</h1>
 				<?php while ( have_posts() ) : the_post(); ?>
 					<li>
 						<article>
@@ -87,11 +88,89 @@
 				</div>
 
 				<?php else: ?>
-				<h2>No meetups scheduled at this time.</h2>
+				<h2>No upcoming events scheduled at this time.</h2>
+				<?php endif; ?>
+				</div>
+		</div>
+
+		<div class="row">
+			<div class="col-sm-12">
+				<ul class="list-unstyled archive-list">
+				<?php
+				$attsPastEvents = array(
+					'title' => NULL,
+					'limit' => 10,
+					'css_class' => NULL,
+					'show_expired' => TRUE,
+					'month' => NULL,
+					'category_slug' => NULL,
+					'order_by' => 'start_date',
+					'sort' => 'DESC',
+					'tax_query' => array(
+						array(
+							'taxonomy' => 'city',
+							'field'    => 'term_id',
+							'terms'    => array( $tax->term_id ),
+						)
+				)
+				);
+				global $wp_query;
+				$wp_query = new EE_Event_List_Query( $attsPastEvents );
+				if ( have_posts() ): ?>
+				<h1>Past Events:</h1>
+				<?php while ( have_posts() ) : the_post(); ?>
+					<?php 
+						$datemunch = espresso_event_date('U','U', $post->ID, FALSE);
+						$pieces = explode(" ", trim($datemunch));
+						$datemunch = $pieces[0];
+
+					if ($datemunch < date('U', time())) { ?>
+					<li>
+						<article>
+							<h2 class="event-h2"><a href="<?php esc_url( the_permalink() ); ?>" title="Permalink to <?php the_title(); ?>" rel="bookmark"><?php the_title(); ?></a></h2>
+							<?php
+							if($post->post_type == 'espresso_events') {
+							?>
+							<div class="event-time-cont">
+								<span class="glyphicon glyphicon-time" aria-hidden="true"></span> <time class="time-hold" datetime="<?php espresso_event_date(); ?>"><span><?php espresso_event_date('l, F j, Y', NULL, $post->ID); ?></span></time>
+							</div>
+							<?php
+							} else {
+							?>
+							<time datetime="<?php the_time( 'Y-m-d' ); ?>" pubdate><?php the_date(); ?> <?php the_time(); ?></time>
+							<?php
+							}
+							?>
+							<div class="row">
+								<div class="col-sm-10">
+									<?php the_excerpt(); ?>
+								</div>
+								<div class="col-sm-2">
+									<a href="<?php esc_url( the_permalink() ); ?>" class="ticket-selector-submit-btn view-details-btn pull-right btn-block text-center">View Details</a>
+								</div>
+							</div>
+
+
+						</article>
+					</li>
+					<?php } ?>
+				<?php endwhile; ?>
+				</ul>
+
+				<div class="post-pagination">
+					<?php
+					// Previous/next page navigation.
+					echo paginate_links();
+					?>
+				</div>
+
+				<?php else: ?>
+				
 				<?php endif; ?>
 				</div>
 
 		</div>
+		
 	</div><!-- /content -->
 </div><!--/middle-->
 <?php get_template_parts( array( 'parts/shared/footer','parts/shared/html-footer' ) ); ?>
