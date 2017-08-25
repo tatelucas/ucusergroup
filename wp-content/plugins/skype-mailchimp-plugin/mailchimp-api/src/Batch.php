6e0b2b92b77324e71f3e68965f91067244140b3b
dwlorimer <dwlorimer@gmail.com>
2017-08-25 11:29:12 -0400

    Add option for Mailchimp updates for entire skype userbase

--- wp-content/plugins/skype-mailchimp-plugin/mailchimp-api/src/Batch.php
+++ wp-content/plugins/skype-mailchimp-plugin/mailchimp-api/src/Batch.php
@@ -15,7 +15,7 @@ class Batch
     private $operations = array();
     private $batch_id;
 
-    public function __construct(MailChimp $MailChimp, $batch_id = null)
+    public function __construct(SkypeMailChimp $MailChimp, $batch_id = null)
     {
         $this->MailChimp = $MailChimp;
         $this->batch_id = $batch_id;
