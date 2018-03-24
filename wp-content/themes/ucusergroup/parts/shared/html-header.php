ac9be015971cbd39ea9220ac42e172702e824b44
dwlorimer <dwlorimer@gmail.com>
2018-03-24 13:39:52 -0400

    Branding change to Skype Usergroups and Teams

--- wp-content/themes/ucusergroup/parts/shared/html-header.php
+++ wp-content/themes/ucusergroup/parts/shared/html-header.php
@@ -10,7 +10,7 @@
 	  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
-		<link rel="shortcut icon" href="<?php echo get_stylesheet_directory_uri(); ?>/img/favicon.ico"/>
+		<link rel="shortcut icon" href="<?php echo get_stylesheet_directory_uri(); ?>/images/favicon/favicon.ico"/>
 
 		<!-- STYLESHEETS -->
 		<?php if ( is_plugin_active( 'memphis-documents-library/memphis-documents.php' ) ) :
